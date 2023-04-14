5a58396aa15d59b038ca009b7583c97d7b8e2eb1
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-14 20:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,35 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
+// Select the output elements
+const outputAll = document.querySelector("#output-all");
+const outputAny = document.querySelector("#output-any");
+
+// Function to fetch the API data and return the time taken
+function fetchData(url) {
+  const startTime = performance.now(); // Start the timer
+  return fetch(url)
+    .then((response) => response.json())
+    .then(() => performance.now() - startTime); // Return the time taken
+}
+
+// Use Promise.all to fetch data from all APIs
+Promise.all(apiUrls.map(fetchData))
+  .then((times) => {
+    // Display the times in the output element
+    times.forEach((time) => {
+      outputAll.innerHTML += `<tr><td>${time.toFixed(2)} ms</td></tr>`;
+    });
+  })
+  .catch((error) => console.error(error));
+
+// Use Promise.any to fetch data from APIs until one succeeds
+Promise.any(apiUrls.map(fetchData))
+  .then((time) => {
+    // Display the time in the output element
+    outputAny.innerHTML += `<tr><td>${time.toFixed(2)} ms</td></tr>`;
+  })
+  .catch((error) => console.error(error));
+
+
 // You can write your code here
